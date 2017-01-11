341e1ac9177da5a78dfb6f0b9a47376c4e91a8ef
Chodanun Srinil <step_nilanam@hotmail.com>
2017-01-11 16:52:08 +0700

    reactnative-redux boilderplate

--- index.ios.js
+++ index.ios.js
@@ -1,10 +1,10 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReduxers, compose } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import createLogger from 'redux-logger';
+import reducer from './App/reducers';
+import AppContainer from './App/containers/AppContainer'
 import {
   AppRegistry,
   StyleSheet,
@@ -12,42 +12,25 @@ import {
   View
 } from 'react-native';
 
-export default class Opcos extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+const loggerMiddleware = createLogger({predicate: (getState,action) => __DEV__});
+
+function configureStore(initialState){
+  const enhancer = compose(
+    applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware,
+    ),
+);
+return createStore(reducer, initialState, enhancer);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+const store = configureStore({})
 
-AppRegistry.registerComponent('Opcos', () => Opcos);
+const App = () => (
+  <Provider store={store}>
+    <Text/>
+  </Provider>
+);
+
+
+AppRegistry.registerComponent('Opcos', () => App);
