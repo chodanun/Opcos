e82efcab119cac80ef1612b831f29b86f9f52156
Chodanun Srinil <step_nilanam@hotmail.com>
2017-04-12 21:44:54 +0700

    create comment components

--- App/containers/Comments.js
+++ App/containers/Comments.js
@@ -16,34 +16,6 @@ export class Comments extends Component {
 	constructor(props) {
         super(props);
         this.state = {
-           data_pos: [{
-           }],
-           data_neg: [{
-           }],
-           options: {
-              width: 300,
-              height: 300,
-              margin: {
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: 0,
-              },
-              r: 120,
-              max: 10,
-              fill: "#2980B9",
-              stroke: "#2980B9",
-              animate: {
-                type: 'oneByOne',
-                duration: 200
-              },
-              label: {
-                fontFamily: 'Arial',
-                fontSize: 14,
-                fontWeight: true,
-                fill: '#34495E'
-              }
-            },
         }
 	}
 
